03c505000b23ddd6c2cd7fc45e6dbb6d61a1c2b1
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-03-29 15:12:18 +0300

    add some tests for task_01

--- task_01/test.cpp
+++ task_01/test.cpp
@@ -1,11 +1,41 @@
 #include "gtest/gtest.h"
 #include "main.hpp"
 #include <cstring>
+#include <iostream>
 
-TEST(check_data, wrong_year)
+TEST(check_sorting, one_elem)
 {
-  ASSERT_EQ(0, 0);
-  ASSERT_EQ(-1, -1);
+	std::vector<int> v = {5};
+	sorting(v.size(), v);
+	ASSERT_THAT(v, ElementsAre(5));
+}
+
+TEST(check_sorting, two_elem)
+{
+	std::vector<int> v = {15, 10};
+	sorting(v.size(), v);
+	ASSERT_THAT(v, ElementsAre(10, 15));
+}
+
+TEST(check_sorting, three_elem)
+{
+	std::vector<int> v = {15, 10, 5};
+	sorting(v.size(), v);
+	ASSERT_THAT(v, ElementsAre(5, 10, 15));
+}
+
+TEST(check_sorting, four_elem)
+{
+	std::vector<int> v = {0, 15, 10, 5};
+	sorting(v.size(), v);
+	ASSERT_THAT(v, ElementsAre(0, 5, 10, 15));
+}
+
+TEST(check_sorting, already_sorted)
+{
+	std::vector<int> v = {0, 5, 10, 15, 20};
+	sorting(v.size(), v);
+	ASSERT_THAT(v, ElementsAre(0, 5, 10, 15, 20));
 }
 
 int main(int argc, char **argv)
