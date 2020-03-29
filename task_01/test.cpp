19488d91028aa4ce2c8f593338a752e60a6e7c32
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-03-29 15:31:40 +0300

    corrected tests

--- task_01/test.cpp
+++ task_01/test.cpp
@@ -3,21 +3,13 @@
 #include <cstring>
 #include <iostream>
 
-#define EXPECT_ARRAY_EQ(TARTYPE, reference, actual, element_count) \
-    {\
-    TARTYPE* reference_ = static_cast<TARTYPE *> (reference); \
-    TARTYPE* actual_ = static_cast<TARTYPE *> (actual); \
-    for(int cmp_i = 0; cmp_i < element_count; cmp_i++ ){\
-      EXPECT_EQ(reference_[cmp_i], actual_[cmp_i]);\
-    }\
-    }
-
 TEST(check_sorting, one_elem)
 {
 	std::vector<int> v = {5};
 	std::vector<int> ac = {5};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 TEST(check_sorting, two_elem)
@@ -25,7 +17,8 @@ TEST(check_sorting, two_elem)
 	std::vector<int> v = {15, 10};
 	std::vector<int> ac = {10, 15};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 TEST(check_sorting, three_elem)
@@ -33,7 +26,8 @@ TEST(check_sorting, three_elem)
 	std::vector<int> v = {15, 10, 5};
 	std::vector<int> ac = {5, 10, 15};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 TEST(check_sorting, four_elem)
@@ -41,7 +35,8 @@ TEST(check_sorting, four_elem)
 	std::vector<int> v = {0, 15, 10, 5};
 	std::vector<int> ac = {0, 5, 10, 15};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 TEST(check_sorting, already_sorted)
@@ -49,7 +44,8 @@ TEST(check_sorting, already_sorted)
 	std::vector<int> v = {0, 5, 10, 15, 20};
 	std::vector<int> ac = {0, 5, 10, 15, 20};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 TEST(check_sorting, back_sorted)
@@ -57,7 +53,8 @@ TEST(check_sorting, back_sorted)
 	std::vector<int> v = {20, 15, 10, 5, 0};
 	std::vector<int> ac = {0, 5, 10, 15, 20};
 	sorting(v.size(), v);
-	EXPECT_ARRAY_EQ(int, v, ac, v.size());
+	for (int i = 0; i < v.size(); i++)
+		EXPECT_EQ(v[i], ac[i]);
 }
 
 int main(int argc, char **argv)
