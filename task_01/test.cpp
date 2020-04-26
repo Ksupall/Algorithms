bdb8966a712b56332ee9b6cdb951d78072dfa3c7
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-04-26 09:40:40 +0300

    add unsighned to size

--- task_01/test.cpp
+++ task_01/test.cpp
@@ -7,7 +7,7 @@ TEST(check_sorting, one_elem)
 {
 	std::vector<int> v = {5};
 	std::vector<int> ac = {5};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
@@ -17,7 +17,7 @@ TEST(check_sorting, two_elem)
 {
 	std::vector<int> v = {15, 10};
 	std::vector<int> ac = {10, 15};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
@@ -27,7 +27,7 @@ TEST(check_sorting, three_elem)
 {
 	std::vector<int> v = {15, 10, 5};
 	std::vector<int> ac = {5, 10, 15};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
@@ -37,7 +37,7 @@ TEST(check_sorting, four_elem)
 {
 	std::vector<int> v = {0, 15, 10, 5};
 	std::vector<int> ac = {0, 5, 10, 15};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
@@ -47,7 +47,7 @@ TEST(check_sorting, already_sorted)
 {
 	std::vector<int> v = {0, 5, 10, 15, 20};
 	std::vector<int> ac = {0, 5, 10, 15, 20};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
@@ -57,7 +57,7 @@ TEST(check_sorting, back_sorted)
 {
 	std::vector<int> v = {20, 15, 10, 5, 0};
 	std::vector<int> ac = {0, 5, 10, 15, 20};
-	int n = v.size();
+	unsigned int n = v.size();
 	sorting(n, v);
 	for (size_t i = 0; i < n; i++)
 		EXPECT_EQ(v[i], ac[i]);
