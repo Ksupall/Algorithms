acfbd9f765ef4300b7bb0366ce21e1d70d01af2e
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-04-26 12:05:08 +0300

    delete new file

--- task_01/main.cpp
+++ task_01/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <vector>
 
+// сортировка вставками
+void sorting(int amount, std::vector<int> &arr)
+{
+	for (int i = 1; i < amount; i++)
+	{
+		int key = arr[i];
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			arr[j] = key;
+			j--;
+		}
+	}
+}
+
 int main()
 {
 	int amount = 0;
