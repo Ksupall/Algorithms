24861de02e247e5fd814ef18b44a1803a3307b99
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-04-26 13:10:29 +0300

    make main.cpp easier

--- task_01/main.cpp
+++ task_01/main.cpp
@@ -4,21 +4,14 @@
 
 int main()
 {
-	int amount = 0;
-	/*
-	std::cout << "Enter amount of elements: ";
-	std::cin >> amount;
-	*/
+	int amount = 5;
 	std::vector<int> arr(amount);
-	/*
-	for (int i = 0; i < amount; ++i)
-		std::cin >> arr[i];
-	*/
-	amount = 5;
 	arr = {-1, 5, 7, -50, 155};
 	sorting(amount, arr);
 	std::cout << std::endl;
+	/*
 	for (int i = 0; i < amount; ++i)
 		std::cout << arr[i] << " ";
+	*/
 	return 0;
 }
