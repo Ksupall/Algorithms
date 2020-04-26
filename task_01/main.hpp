b69115f8eebe335987524782dfe8385522868261
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-04-26 09:52:46 +0300

    deleted -pthread and corrected header

--- task_01/main.hpp
+++ task_01/main.hpp
@@ -1,6 +1,6 @@
 #ifndef __MAIN_HPP__
 #define __MAIN_HPP__
 
-void sorting(int amount, std::vector<int> &arr);
+void sorting(int amount, std::vector<int, std::allocator<int> >&);
 
 #endif // __MAIN_HPP__
