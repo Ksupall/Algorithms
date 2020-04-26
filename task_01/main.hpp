849004e68a12569acf667f93b36a3e476005238b
Ksupall <KsenyaPolyakovaaa@mail.ru>
2020-04-26 12:10:23 +0300

    added new files^ sorting.cpp and sorting.hpp

--- task_01/main.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __MAIN__H__
-#define __MAIN__H__
-
-void sorting(int, std::vector<int, std::allocator<int> >&);
-
-#endif // __MAIN__H__
---
+++ task_01/sorting.hpp
@@ -0,0 +1,6 @@
+#ifndef __SORTING__H
+#define __SORTING__H
+
+void sorting(int amount, std::vector<int> &array);
+
+#endif // __SORTING__H
