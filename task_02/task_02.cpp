64e0c89e4fad6e519e3841b7fa0ea41c23532b60
Polyakova Ksenya <KsenyaPolyakovaaa@mail.ru>
2020-06-20 23:16:26 +0300

    previous changes

--- task_02/task_02.cpp
+++ task_02/task_02.cpp
@@ -2,49 +2,62 @@
 #include <vector>
 
 // сортировка слиянием
-void Merge(std::vector<int> A, int first, int last)
+void Merge(std::vector<int> &arr, int first, int middle, int last)
 {
-	int middle, start, final, j;
-	std::vector<int> mas(100);
-	middle = (first + last) / 2; //вычисление среднего элемента
-	start = first; //начало левой части
-	final = middle + 1; //начало правой части
-	for (j = first; j <= last; j++) //выполнять от начала до конца
-		if ((start <= middle) && ((final > last) || (A[start] < A[final])))
-		{
-			mas[j] = A[start];
-			start++;
-		}
+	int n1 = middle - first + 1;
+	int n2 = last - middle;
+	std::vector<int> b(n1 + 1);
+	std::vector<int> c(n2 + 1);
+	int j = 0;
+	for (int i = first; i <= middle; i++)
+	{
+		b[j] = arr[i];
+		j++;
+	}
+	/*
+	for (int i = 0; i < n1 + 1; i++)
+		std::cout << b[i] << " ";
+	*/
+	j = 0;
+	for (int i = middle + 1; i <= last; i++)
+	{
+		c[j] = arr[i];
+		j++;
+	}
+	b[n1 + 1] = 100000000;
+	c[n2 + 1] = 100000000;
+	int i = 0;
+	j = 0;
+	
+	for (int k = first; k <= last; k++)
+	{
+		if (b[i] <= c[j] )
+			arr[k] = b[i++];
 		else
-		{
-			mas[j] = A[final];
-			final++;
-		}
-	//возвращение результата в список
-	for (j = first; j <= last; j++) A[j] = mas[j];
+			arr[k] = c[j++];
+	}
 };
 
-void MergeSort(std::vector<int> A, int first, int last)
+void MergeSort(std::vector<int> &arr, int first, int last)
 {
 	if (first < last)
 	{
-		MergeSort(A, first, (first + last) / 2); //сортировка левой части
-		MergeSort(A, (first + last) / 2 + 1, last); //сортировка правой части
-		Merge(A, first, last); //слияние двух частей
+		int middle = (first + last) / 2;
+		MergeSort(arr, first, middle); //сортировка левой части
+		MergeSort(arr, middle + 1, last); //сортировка правой части
+		Merge(arr, first, middle, last); //слияние двух частей
 	} 
 }
 
 int main()
 {
-	int amount = 0;
-	std::cout << "Enter amount of elements: ";
-	std::cin >> amount;
+	int amount = 5;
 	std::vector<int> arr(amount);
-	for (int i = 0; i < amount; ++i)
-		std::cin >> arr[i];
-	MergeSort(arr, 1, amount);
-	std::cout << std::endl;
+	arr = {-1, 5, 7, -50, 6};
+	MergeSort(arr, 0, amount);
+	
 	for (int i = 0; i < amount; ++i)
 		std::cout << arr[i] << " ";
+	
 	return 0;
 }
